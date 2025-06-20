c74fbfc300f716f4885c8f85ac485534e7b5543f
Алексей Панфилов <aleksei.panfilov@nordclan.com>
2025-06-21 01:44:44 +0400

    migration Fix

--- Restorator.DataAccess/Migrations/20250620214426_AddedRestaurantImages.Designer.cs
+++ Restorator.DataAccess/Migrations/20250620214426_AddedRestaurantImages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restorator.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using Restorator.DataAccess.Data;
 namespace Restorator.DataAccess.Migrations
 {
     [DbContext(typeof(RestoratorDbContext))]
-    partial class RestoratorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250620214426_AddedRestaurantImages")]
+    partial class AddedRestaurantImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Restorator.DataAccess/Migrations/20250620214426_AddedRestaurantImages.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restorator.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRestaurantImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantImage_Restaurants_RestaurantId",
+                table: "RestaurantImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantImage",
+                table: "RestaurantImage");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantImage",
+                newName: "RestaurantImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantImage_RestaurantId",
+                table: "RestaurantImages",
+                newName: "IX_RestaurantImages_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantImages",
+                table: "RestaurantImages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantImages_Restaurants_RestaurantId",
+                table: "RestaurantImages",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantImages_Restaurants_RestaurantId",
+                table: "RestaurantImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantImages",
+                table: "RestaurantImages");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantImages",
+                newName: "RestaurantImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantImages_RestaurantId",
+                table: "RestaurantImage",
+                newName: "IX_RestaurantImage_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantImage",
+                table: "RestaurantImage",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantImage_Restaurants_RestaurantId",
+                table: "RestaurantImage",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+    }
+}
